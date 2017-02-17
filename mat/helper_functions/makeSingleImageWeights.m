8b566fc92b34f05764d2694159f784c328c32d41
Isaac Pedisich <iped@huntsman-ve565-1109.apn.wlan.upenn.edu>
2017-02-17 16:07:49 -0500

    deleted old matlab code

--- mat/helper_functions/makeSingleImageWeights.m
+++
@@ -1,9 +0,0 @@
-function [ weights ] = makeSingleImageWeights( image, settings )
-%weights = MAKESINGLEIMAGEWEIGHTS(image) multiplies an image by its
-%    transpose, getting the weight matrix associated with just that image
-
-this_image_flat = image(:);
-weights = this_image_flat*this_image_flat';
-weights = int8(weights);
-end
-
